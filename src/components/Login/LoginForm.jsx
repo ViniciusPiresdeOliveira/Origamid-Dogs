59edddf3ee4133a35c850758ad3910f616d7ee60
Vinicius Pires <piresvinicius35@gmail.com>
2023-12-12 23:35:12 -0300

    feat(LoginForm): use new component

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Button from '../Forms/Button'
+import Input from '../Forms/Input'
 
 const LoginForm = () => {
 
@@ -27,9 +29,9 @@ const LoginForm = () => {
         <section>
             <h1>Login</h1>
             <form action="" onSubmit={handleSubmit}>
-                <input type="text" onChange={({ target }) => setUsername(target.value)} value={username} />
-                <input type="text" onChange={({ target }) => setPassowrd(target.value)} value={password} />
-                <button>Entrar</button>
+                <Input label="Usuário" type="text" name="username" />
+                <Input label="Senha" type="password" name="password" />
+                <Button>Entrar</Button>
             </form>
             <Link to='/login/criar'>Cadastro</Link>
         </section>
