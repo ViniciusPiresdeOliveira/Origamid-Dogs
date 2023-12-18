07b94b094c9c74b2b8b86c9b3a4df4e7e4a7de8f
Vinicius Pires <piresvinicius35@gmail.com>
2023-12-17 23:09:49 -0300

    style: add new style

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import { UserContext } from '../../context/UserContext'
+import styles from './Login.module.css'
 import LoginCreate from './LoginCreate'
 import LoginForm from './LoginForm'
 import LoginPasswordLost from './LoginPasswordLost'
@@ -16,14 +17,16 @@ const Login = () => {
       }
 
     return (
-        <div>
-            <Routes>
-                <Route path='/' element={<LoginForm />} />
-                <Route path='criar' element={<LoginCreate />} />
-                <Route path='perdeu' element={<LoginPasswordLost />} />
-                <Route path='resetar' element={<LoginPasswordReset />} />
-            </Routes>
-        </div>
+        <section className={styles.login}>
+            <div className={styles.forms}>
+                <Routes>
+                    <Route path='/' element={<LoginForm />} />
+                    <Route path='criar' element={<LoginCreate />} />
+                    <Route path='perdeu' element={<LoginPasswordLost />} />
+                    <Route path='resetar' element={<LoginPasswordReset />} />
+                </Routes>
+            </div>
+        </section>
     )
 }
 
